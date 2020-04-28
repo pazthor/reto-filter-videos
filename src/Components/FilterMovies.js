81dac80676baf5e8f75d0a761d00e019827fde30
Julio <pastor@indava.com>
2020-04-28 11:18:39 -0500

    Add filter component

--- src/Components/FilterMovies.js
+++ src/Components/FilterMovies.js
@@ -1,20 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { getMovieInfo } from "../Store/APIMocks";
+import SearchBar from "./SearchBar";
+import MovieCard from "./MovieCard";
+import { CardGroup, CardColumns } from "react-bootstrap";
+import MovieInfoModal from "./MovieInfoModal";
 
+function FilterMovies({ movies }) {
+  const [moviesState, setMoviesState] = useState(movies);
+  const [movieTitle, setMovieTitle] = useState();
+  const [movieDescription, setMovieDescription] = useState();
+  const [modalShow, setModalShow] = useState();
+  const [movieImg, setMovieImg] = useState();
+  const onGetMovieInfo = async (id) => {
+    const movieDTO = await getMovieInfo(id);
+    setMovieTitle(movieDTO.title);
+    setMovieDescription(movieDTO.description);
+    setMovieImg(movieDTO.image_small);
+    setModalShow(true);
+  };
 
-
-function FilterMovies({movies}) {
   const listMoviesComponent = (movies) => {
-    return movies.map(m => {
-      return m.title
-    })
-  }
-  
+    return movies.map((m) => {
+      return (
+        <MovieCard
+          id={m.id}
+          title={m.title}
+          img={m.image_small}
+          description={m.description}
+          handleMovieInfo={onGetMovieInfo}
+        />
+      );
+    });
+  };
+
+  const filterItems = (arr, query) => {
+    if (!query) return movies;
+    return arr.filter(
+      (el) => el.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    );
+  };
+
+  const onFilter = (word) => {
+    console.log(word);
+    const result = filterItems(moviesState, word);
+    setMoviesState(result);
+  };
+
   return (
     <>
-    { listMoviesComponent(movies)}
+      <SearchBar handleOnClick={onFilter} />
+      <CardColumns>{listMoviesComponent(moviesState)}</CardColumns>
+      <MovieInfoModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        title={movieTitle}
+        description={movieDescription}
+        img={movieImg}
+      />
     </>
-  )
-  ;
+  );
 }
 
 export default FilterMovies;
