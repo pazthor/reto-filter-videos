f7bfb46a422f1c40048b2bab13fd3a1c0830937e
Julio <pastor@indava.com>
2020-04-28 11:18:25 -0500

    add search bar component

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
+import { InputGroup, FormControl, Button } from "react-bootstrap";
 
-
-
-function SearchBar() {
+function SearchBar({ handleOnClick }) {
+  const [input, setInput] = useState();
   return (
-  <></>
-  )
-  ;
+    <>
+      <InputGroup onChange={(e) => setInput(e.target.value)} className="mb-3">
+        <FormControl
+          placeholder="Escribe la película"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+        <InputGroup.Append>
+          <Button
+            variant="outline-secondary"
+            onClick={() => handleOnClick(input)}
+          >
+            Filtrar
+          </Button>
+        </InputGroup.Append>
+      </InputGroup>
+    </>
+  );
 }
 
 export default SearchBar;
