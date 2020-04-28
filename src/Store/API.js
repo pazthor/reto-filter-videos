6d1a46d3718f2765fc25b8889c8f1dfa4e6b6799
Julio <pastor@indava.com>
2020-04-28 11:17:29 -0500

    Add update api and api mocks

--- src/Store/API.js
+++ src/Store/API.js
@@ -1,51 +1,56 @@
 import { FetchGet } from "../Utils/FetchGet";
 import { URLS } from "../Utils/URLS";
 
-
-
-export const getListadoInfinito = (id)  => {
-
+export const getListadoInfinito = (id) => {
   return FetchGet(`${URLS.getInfoListadoInfinito}&node=${id}`)
-  .then(res => {      
-    console.log(res)
-    return res
-  })
-  .catch(err => {
-    console.log(err)
-    return []
-  });
-}
+    .then((res) => {
+      console.log(res);
+      return res;
+    })
+    .catch((err) => {
+      console.log(err);
+      return [];
+    });
+};
 
-
-export const getMovies = async  (categoria) => {
+export const getMovies = async (categoria) => {
   const DTO = await getListadoInfinito(categoria);
   try {
-    const moduleArray =  DTO.response.modules?.module;
-    const componentArray =moduleArray[0]?. components?.component; 
-    const listadoInfinitoDTO = componentArray.find(p=>p.type === 'Listadoinfinito' )
-    
+    const moduleArray = DTO.response.modules?.module;
+    const componentArray = moduleArray[0]?.components?.component;
+    const listadoInfinitoDTO = componentArray.find(
+      (p) => p.type === "Listadoinfinito"
+    );
+
     const urlString = listadoInfinitoDTO?.properties.url;
     console.log(urlString);
-    const movies= await getMoviesList(urlString)
-  
-    return movies;    
+    const movies = await getMoviesList(urlString);
+
+    return movies;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-
-
-}
+};
 
 const getMoviesList = (url) => {
-  
-  return FetchGet(`${URLS.getListMovies+url}`)
-  .then(res => {      
-    console.log(res.entry?.response?.groups)
-    return res.entry?.response.groups
-  })
-  .catch(err => {
-    console.log(err)
-    return []
-  });
-}
-
+  return FetchGet(`${URLS.getListMovies + url}`)
+    .then((res) => {
+      console.log(res.entry?.response?.groups);
+      return res.entry?.response.groups;
+    })
+    .catch((err) => {
+      console.log(err);
+      return [];
+    });
+};
+export const getMovieInfo = async (id) => {
+  return FetchGet(`${URLS.getMovieInfo + id}`)
+    .then((res) => {
+      console.log(res.response.group.common);
+      return res.response.group.common;
+    })
+    .catch((err) => {
+      console.log(err);
+      return [];
+    });
+};
