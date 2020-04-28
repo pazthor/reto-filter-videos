71b87defdfd9f36c74c95cb06ecb1e3dc2d6c358
Julio <pastor@indava.com>
2020-04-28 11:19:08 -0500

    add styles and useState SetMovies

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,31 +1,28 @@
-import React, { useState, useContext } from 'react';
-import SectionMovies from './SectionMovies';
-import FilterMovies from './FilterMovies';
-import { store } from '../Store/StoreContext';
-import {getMovies} from '../Store/API'
+import React, { useState, useContext } from "react";
+import SectionMovies from "./SectionMovies";
+import FilterMovies from "./FilterMovies";
+import { store } from "../Store/StoreContext";
+import { getMovies } from "../Store/APIMocks";
 
 function Dashboard() {
   const [isSeccionState, setIsSeccionState] = useState(false);
-  const { setMovies, movies} = useContext(store);
-  
- 
+  const { setMovies, movies } = useContext(store);
+
   const handledOnClick = async (categoria) => {
-    // endpoint categoria     
-    
-    const moviesDto =  await getMovies(categoria)  
-    setMovies(moviesDto)
+    const moviesDto = await getMovies(categoria);
+    setMovies(moviesDto);
     setIsSeccionState(!!moviesDto);
-  }
-  
+  };
+
   return (
-  <> 
-  {isSeccionState?
-  <FilterMovies movies={movies} />:
-  <SectionMovies onClickCategoria={handledOnClick} />}
-  
-  </>
-  )
-  ;
+    <>
+      {isSeccionState ? (
+        <FilterMovies movies={movies} />
+      ) : (
+        <SectionMovies onClickCategoria={handledOnClick} />
+      )}
+    </>
+  );
 }
 
 export default Dashboard;
