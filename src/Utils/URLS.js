6cee7ec8bc282f0d4e8ac2258719eb3fc48a0723
Julio <pastor@indava.com>
2020-04-28 13:16:32 -0500

    add path api

--- src/Utils/URLS.js
+++ src/Utils/URLS.js
@@ -2,7 +2,10 @@ export const BASENAME_URL = "https://mfwkweb-api.clarovideo.net/";
 export const URLS = {
   getInfoListadoInfinito: `${BASENAME_URL}/services/cms/level?api_version=v5.86&authpn=webclient&authpt=tfg1h3j4k6fd7&format=json&region=mexico&device_id=web&device_category=web&device_model=web&device_type=web&device_manufacturer=generic&HKS=9s5hqq76r3g6sg4jb90l38us52&isCacheable=true&domain=https%3A%2F%2Fmfwkweb-api.clarovideo.net%2F&origin=https%3A%2F%2Fwww.clarovideo.com%2F&user_id=22822863`,
   getListMovies: `${BASENAME_URL}`,
+  getApi:`api_version=v5.86&authpn=webclient&authpt=tfg1h3j4k6fd7&format=json&region=mexico&device_id=web&device_category=web&device_model=web&device_type=web&device_manufacturer=generic&HKS=9s5hqq76r3g6sg4jb90l38us52`,
   getMovieInfo: `${BASENAME_URL}services/content/data?device_id=web&device_category=web&device_model=web&device_type=
   web&format=json&device_manufacturer=generic&authpn=webclient&authpt=tfg1h3j4k6fd7&api_version=v5.86&region=mexico&HKS
   =9s5hqq76r3g6sg4jb90l38us52&user_id=22822863&group_id=`,
 };
+
+
