c7b605004196eff9c88d340a1ed20700e784bc96
Julio <pastor@indava.com>
2020-04-29 12:01:08 -0500

    Remove constant

--- src/Store/StoreContext.js
+++ src/Store/StoreContext.js
@@ -7,31 +7,14 @@ const initialState = {
 const store = createContext(initialState);
 const { Provider } = store;
 const actions = {
-  SET_MOVIES: "SET_MOVIES",
-  SET_MONSTER: "SET_MONSTER",
-  SET_GAME: "SET_GAME",
-  SET_CARDLIST: "SET_CARDLIST",
-  SET_MONSTEREFFECT: "SET_MONSTEREFFECT",
-  SET_CARDSELECTED: "SET_CARDSELECTED",
-  RESET: "RESET",
+  SET_MOVIES: "SET_MOVIES",  
+  
 };
 
 function reducer(state, action) {
   switch (action.type) {
     case actions.SET_MOVIES:
       return { ...state, movies: action.value };
-    case actions.SET_MONSTER:
-      return { ...state, monster: action.value };
-    case actions.SET_GAME:
-      return { ...state, gameInfo: action.value };
-    case actions.SET_CARDLIST:
-      return { ...state, cardList: action.value };
-    case actions.SET_MONSTEREFFECT:
-      return { ...state, monsterEffect: action.value };
-    case actions.SET_CARDSELECTED:
-      return { ...state, cardSelected: action.value };
-    case actions.RESET:
-      return { ...state, ...initialState };
     default:
       return state;
   }
@@ -40,33 +23,11 @@ const StateProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const value = {
     movies: state.movies,
-    monster: state.monster,
-    gameInfo: state.gameInfo,
-    cardList: state.cardList,
-    monsterEffect: state.monsterEffect,
-    cardSelected: state.cardSelected,
 
     setMovies: (value) => {
       dispatch({ type: actions.SET_MOVIES, value });
     },
-    setMonster: (value) => {
-      dispatch({ type: actions.SET_MONSTER, value });
-    },
-    setMonsterEffect: (value) => {
-      dispatch({ type: actions.SET_MONSTEREFFECT, value });
-    },
-    setGame: (value) => {
-      dispatch({ type: actions.SET_GAME, value });
-    },
-    setCardList: (value) => {
-      dispatch({ type: actions.SET_CARDLIST, value });
-    },
-    setCardSelected: (value) => {
-      dispatch({ type: actions.SET_CARDSELECTED, value });
-    },
-    reset: () => {
-      dispatch({ type: actions.RESET });
-    },
+  
   };
   return <Provider value={value}>{children}</Provider>;
 };
